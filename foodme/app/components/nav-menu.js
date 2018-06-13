00283b8851eff62c9cf7e44d034c6f02585cba3b
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-06-13 20:58:58 +0200

    Fill 'nav-menu' component files

--- foodme/app/components/nav-menu.js
+++ foodme/app/components/nav-menu.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+  isMenuOpened: false,
+
+  actions: {
+    toggleMenu() {
+      this.toggleProperty('isMenuOpened');
+    }
+  }
+
 });
