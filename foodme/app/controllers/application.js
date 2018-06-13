0acd3dcd78786206c2c800d40a622b3848a7a9a6
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-06-13 20:59:12 +0200

    Add properties to application controller

--- foodme/app/controllers/application.js
+++ foodme/app/controllers/application.js
@@ -1,4 +1,16 @@
 import Controller from '@ember/controller';
 
+// OJO: Al definir 'menuLinks', ESLint nos avisará de que estamos incumpliendo la regla 'avoid-leaking-state-in-components'.
+// Para no enredar el workshop vamos a dejarlo así, pero la forma correcta de hacerlo es esta:
+// https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/avoid-leaking-state-in-components.md
+
 export default Controller.extend({
+
+  appName: 'FoodMe',
+
+  menuLinks: [{
+    name: 'Restaurantes',
+    url: 'restaurants'
+  }]
+
 });
