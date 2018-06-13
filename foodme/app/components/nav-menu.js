6b6255ac82fe1d40e75650e641c9a5fc67895171
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-06-13 21:58:36 +0200

    Add language selector to nav menu

--- foodme/app/components/nav-menu.js
+++ foodme/app/components/nav-menu.js
@@ -1,12 +1,36 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
+import { observer } from '@ember/object';
+
+const languages = [{
+  name: 'Español',
+  code: 'es'
+}, {
+  name: 'English',
+  code: 'en'
+}];
 
 export default Component.extend({
 
   isMenuOpened: false,
 
+  i18n: inject(),
+
+  languages,
+
+  selectedLanguage: languages[1],
+
+  onSelectedLanguageChange: observer('selectedLanguage', function() {
+    this.set('i18n.locale', this.get('selectedLanguage.code'));
+  }),
+
   actions: {
     toggleMenu() {
       this.toggleProperty('isMenuOpened');
+    },
+
+    modifyLanguage(languageCode){
+      this.get('i18n').set('locale', languageCode);
     }
   }
 
