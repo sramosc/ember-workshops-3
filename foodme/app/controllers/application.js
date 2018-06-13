30733240bcef7c802c4867a9646002ed43b579b3
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-06-13 21:58:08 +0200

    Disable leaking rule on application controller

--- foodme/app/controllers/application.js
+++ foodme/app/controllers/application.js
@@ -1,5 +1,7 @@
 import Controller from '@ember/controller';
 
+/* eslint-disable ember/avoid-leaking-state-in-ember-objects */
+
 // OJO: Al definir 'menuLinks', ESLint nos avisará de que estamos incumpliendo la regla 'avoid-leaking-state-in-components'.
 // Para no enredar el workshop vamos a dejarlo así, pero la forma correcta de hacerlo es esta:
 // https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/avoid-leaking-state-in-components.md
