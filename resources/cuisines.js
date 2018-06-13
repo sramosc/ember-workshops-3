3657af21b1802438deb574123e355351126435fa
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-06-13 19:13:06 +0200

    Add resources

---
+++ resources/cuisines.js
@@ -0,0 +1,15 @@
+const CUISINE_OPTIONS = [
+	{ name: 'african', title: 'African' },
+	{ name: 'american', title: 'American' },
+	{ name: 'barbecue', title: 'Barbecue' },
+	{ name: 'cafe', title: 'Cafe' },
+	{ name: 'chinese', title: 'Chinese' },
+	{ name: 'czech/slovak', title: 'Czech / Slovak' },
+	{ name: 'german', title: 'German' },
+	{ name: 'indian', title: 'Indian' },
+	{ name: 'japanese', title: 'Japanese' },
+	{ name: 'mexican', title: 'Mexican' },
+	{ name: 'pizza', title: 'Pizza' },
+	{ name: 'thai', title: 'Thai' },
+	{ name: 'vegetarian', title: 'Vegetarian' }
+];
---
+++ resources/translations/en/translations.js
@@ -0,0 +1,44 @@
+export default {
+    // "some.translation.key": "Text for some.translation.key",
+    //
+    // "a": {
+    //   "nested": {
+    //     "key": "Text for a.nested.key"
+    //   }
+    // },
+    //
+    // "key.with.interpolation": "Text with {{anInterpolation}}"
+
+	"about": "About",
+
+	"cuisine": "Cuisine",
+
+	"filter": "Filter",
+
+	"filterRestaurants": "Filter restaurants",
+
+	"foundRestaurants": {
+		"zero": "No restaurants found!",
+		"one": "{{count}} restaurant found!",
+   		"other": "{{count}} restaurants found!"
+	},
+
+	"language": "Language",
+
+	"rating": "Rating",
+
+	"restaurants": "Restaurants",
+
+	"selectCuisins": "Select some cuisines...",
+
+	"weekDays": {
+		"sunday": "sunday",
+		"monday": "monday",
+		"tuesday": "tuesday",
+		"wednesday": "wednesday",
+		"thursday": "thursday",
+		"friday": "friday",
+		"saturday": "saturday"
+	}
+
+};
