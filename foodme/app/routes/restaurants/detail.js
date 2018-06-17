f0a84d289e200381aa943d68a0e948bbceee9104
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-06-17 21:30:32 +0200

    Add Google Maps button to 'restaurants/detail'

--- foodme/app/routes/restaurants/detail.js
+++ foodme/app/routes/restaurants/detail.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
+import OpenMapMixin from 'foodme/mixins/open-map';
 
-export default Route.extend({
+export default Route.extend(OpenMapMixin, {
 
     model({id}){
         const parentModel = this.modelFor('restaurants') || [];
