f90db98e558ddae9ebbad6eee2a389ce0e0473b5
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-06-17 19:48:47 +0200

    Fix helper 'restaurant-img'

--- foodme/app/helpers/restaurant-img.js
+++ foodme/app/helpers/restaurant-img.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function restaurantImg(params/*, hash*/) {
-  return `img/restaurants/${params}.jpg`;
+  return `/img/restaurants/${params}.jpg`;
 }
 
 export default helper(restaurantImg);
