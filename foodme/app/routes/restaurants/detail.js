f68f2a173fd32345a2bf1afd01a9b12b6daa26bc
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-06-17 19:44:24 +0200

    Add html for 'detail'

--- foodme/app/routes/restaurants/detail.js
+++ foodme/app/routes/restaurants/detail.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    model({id}){
+        const parentModel = this.modelFor('restaurants') || [];
+        return parentModel.findBy('id', id);
+    }
+
 });
