db94c487f1997f760f57fc530129ca0d20a862b2
Sergio Ramos <sramosc@gmail.com>
2020-10-01 13:22:13 +0200

    using restaurants.json as model

--- foodme/app/routes/restaurants.js
+++ foodme/app/routes/restaurants.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    async model() {
+        let response = await fetch('/api/restaurants.json');
+        let parsed = await response.json();
+        return parsed;
+    }
 });
